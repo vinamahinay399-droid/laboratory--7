211ded65e81b10358d35a5ce34f142b773c164ff
vinamahinay399-droid <vinamahinay399@gmail.com>
2025-11-19 12:17:28 +0800

    Add Product class with Lombok annotations

---
+++ product.java
@@ -0,0 +1,14 @@
+package com.example.laboratory7;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    private Long id;
+    private String name;
+    private Double price;
+}
